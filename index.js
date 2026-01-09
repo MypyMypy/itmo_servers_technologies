0e02d184d4626859afdcd113bd113d142c3041b6
Danil Bogdanchikov <bogdanchikovdan@yandex.ru>
2026-01-10 00:51:13 +0300

    fix ex 7(9)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import pug from "pug";
 
 import appSrc from "./app.js";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "0.0.0.0";
 const LOGIN = "mypymypy";
 const uuid = "8155ee0b-ebea-4a53-93fe-a9ae47fb83ee";
